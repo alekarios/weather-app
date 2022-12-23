87dd2271d28ed15bdd82492feab9b874e657a2e3
Alexandros Theofilis <alexandros.theofilis@verlabs.local>
2022-12-23 17:47:25 +0200

    beautify code

--- src/components/sideForecast/sideForecast.jsx
+++ src/components/sideForecast/sideForecast.jsx
@@ -12,22 +12,25 @@ const SideForecast = () => {
 	return (
 		<div className='sideForecast-container'>
 			<p>3-Day Forecast </p>
-            {forecastData.map(({ date, day }) => {
-                const dayIndex = new Date(date).getDay()
-                const nameDay = days.find(({ id }) => id === dayIndex ).name
-                return (
-                    <>
-                        <div
-                            key={date}
-                            className='sideForecast-container__items'>
-                            <p>{ nameDay }</p>
-                            <img src={ day.condition.icon} alt='temp' />
-                            <p>{ date }</p>
-                        </div>
-                        <hr />
-                    </>
-                )
-            } )}
+			{forecastData.map(({ date, day }) => {
+				const dayIndex = new Date(date).getDay();
+				const nameDay = days.find(({ id }) => id === dayIndex).name;
+				return (
+					<>
+						<div
+							key={date}
+							className='sideForecast-container__items'>
+							<p>{nameDay}</p>
+							<img
+								src={day.condition.icon}
+								alt='temp'
+							/>
+							<p>{date}</p>
+						</div>
+						<hr />
+					</>
+				);
+			})}
 		</div>
 	);
 };
--- src/components/todayDashboad/todayDashboard.jsx
+++ src/components/todayDashboad/todayDashboard.jsx
@@ -4,18 +4,28 @@ import tryGet from 'src/utils/tryGet';
 
 const TodayDashboard = ({ data }) => {
 	const filteredData = tryGet(
-		() => data.forecastday[0].hour.filter((element, index) => Math.abs(index % 2) === 1),
+		() =>
+			data.forecastday[0].hour.filter(
+				(element, index) => Math.abs(index % 2) === 1
+			),
 		[]
 	);
-    return <div className='todayDashboard-container'>
-        {filteredData.map(({ condition, time, temp_c }) => (
-            <div key={ time } className='todayDashboard-container__hour'>
-                <p>{ `${temp_c} °C` }</p>
-                <img src={condition.icon} alt='hour' />
-                <p>{ new Date(time).toLocaleTimeString() }</p>
-            </div>
-        ))}
-    </div>;
+	return (
+		<div className='todayDashboard-container'>
+			{filteredData.map(({ condition, time, temp_c }) => (
+				<div
+					key={time}
+					className='todayDashboard-container__hour'>
+					<p>{`${temp_c} °C`}</p>
+					<img
+						src={condition.icon}
+						alt='hour'
+					/>
+					<p>{new Date(time).toLocaleTimeString()}</p>
+				</div>
+			))}
+		</div>
+	);
 };
 
 export default TodayDashboard;
